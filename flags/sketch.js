46e23048afa94e3f47abdc76f18c16ae628b8b02
Elias Westerberg <elias.westerberg@gmail.com>
2019-01-14 08:42:42 +0100

    Download button

--- flags/sketch.js
+++ flags/sketch.js
@@ -271,3 +271,16 @@ function setup () {
 function draw () {
 
 }
+
+function uuidv4 () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16 | 0
+    let v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+
+function download () {
+  let uid = uuidv4().substr(0, 4)
+  saveCanvas('flag-' + uid, 'png')
+}
